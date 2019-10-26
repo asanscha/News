e94b16b4f8dcafa0d9246d6f59989703fe95363d
Alex Sanschagrin <asanscha@gmail.com>
2019-10-26 15:22:57 -0700

    working routes

--- server.js
+++ server.js
@@ -27,28 +27,27 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/NewsApp", { useNewUrlParser: true });
 
 // Routes
 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with axios
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("https://www.npr.org/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
-
+    console.log($)
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $(".story-text h3").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
-        .children("a")
         .text();
       result.link = $(this)
-        .children("a")
+        .parent("a")
         .attr("href");
 
       // Create a new Article using the `result` object built from scraping
